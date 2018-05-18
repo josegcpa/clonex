bb49662f86eae396df262d71726189bbf6e37e37
Moritz Gerstung <moritz.gerstung@ebi.ac.uk>
2018-05-18 20:00:03 +0100

    Make number of drivers user input
    Removed some unnecessary code

--- clonex.c
+++ clonex.c
@@ -20,7 +20,7 @@ Author  : Niko Beerenwinkel
 #include <omp.h>
 
 
-#define d 1000000 // dimension (number of loci)
+//#define genes 1000000 // dimension (number of loci)
 #define MAX_GENOTYPES 1000000  // max. 10^6
 #define MAX_K 1000  // max. no. of mutations per genotype
 
@@ -42,186 +42,15 @@ unsigned int geno_count[MAX_GENOTYPES];
 int N_g;  // current number of genotypes (make sure that N_g <= MAX_GENOTYPES)
 double fitness[MAX_K+1];
 
-int sum_obs[d];
 int k_abs_freq[MAX_K+1];
 double k_rel_freq[MAX_K+1];
 
 gsl_rng *RNG;  // random number generator
 
 
-int hamming_distance(i1, i2)
-{
-	int j, l, g1[d], g2[d];
-
-	for (j=0; j<d; j++)
-		g1[j] = g2[j] = 0;
-
-	for (l=0; l<genotype[i1].k; l++)
-		g1[genotype[i1].mutation[l]] = 1;
-	for (l=0; l<genotype[i2].k; l++)
-		g1[genotype[i2].mutation[l]] = 1;
-
-	int dist = 0;
-	for (j=0; j<d; j++)
-		dist += (g1[j] != g2[j]);
-
-	return dist;
-}
-
-
-double expected_hamming_distance()
-{
-	int i, i1, i2;
-
-	double rel_freq[MAX_GENOTYPES];
-	int N = 0;
-	for (i=0; i<N_g; i++)
-		N += genotype[i].count;
-	for (i=0; i<N_g; i++)
-		rel_freq[i] = (double) genotype[i].count / (double) N;
-
-	double dist = 0.0;
-	for (i1=0; i1<N_g; i1++)
-		for (i2=i1+1; i2<N_g; i2++)
-			dist += rel_freq[i1] * rel_freq[i2] * (double) hamming_distance(i1, i2);
-
-	return dist;
-}
 
 
 
-double detect_mutations(int *k_min, int *k_max, double *k_mean, int *k_median, int *k_obs, double *k_plus, double *k_minus, double *prob_add, double *homo, double *div, double delta)
-{
-	int i, j, l;
-	int k, count;
-
-	for (j=0; j<d; j++)
-		sum_obs[j] = 0;
-
-
-	for (k=0; k<=MAX_K; k++)
-	{
-		k_abs_freq[k] = 0;
-		k_rel_freq[k] = 0.0;
-	}
-
-	//#pragma omp parallel for private(k,count)
-	for (i=0; i<N_g; i++)
-	{
-		k = genotype[i].k;
-		count = genotype[i].count;
-
-		k_abs_freq[k] += count;
-		for (l=0; l<k; l++)
-			sum_obs[genotype[i].mutation[l]] += count;
-	}
-
-	int N = 0;
-	int max_freq = 0;
-	for (k=0; k<=MAX_K; k++)
-	{
-		N += k_abs_freq[k];
-		if ((k == 0) || (k_abs_freq[k] > max_freq))
-		{
-			max_freq = k_abs_freq[k];
-			*k_median = k;
-		}
-	}
-
-
-	int k_median_count = 0;
-	int dominant_count = 0;
-	int dominant_type;
-	for (i=0; i<N_g; i++)
-	{
-		if (genotype[i].k == *k_median)
-		{
-			k_median_count += genotype[i].count;
-			if ((dominant_count == 0) || (genotype[i].count > dominant_count))
-			{
-				dominant_type = i;
-				dominant_count = genotype[i].count;
-			}
-		}
-	}
-	*homo = (double) dominant_count / (double) k_median_count;
-
-	*div = expected_hamming_distance();
-
-	for (k=0; k<=MAX_K; k++)
-		k_rel_freq[k] = (double) k_abs_freq[k] / (double) N;
-
-	*k_min = 0;
-	while (k_abs_freq[*k_min] == 0)
-		(*k_min)++;
-
-	*k_max = MAX_K;
-	while (k_abs_freq[*k_max] == 0)
-		(*k_max)--;
-
-	*k_mean = 0.0;
-	for (k=0; k<=MAX_K; k++)
-		*k_mean += k_rel_freq[k] * (double) k;
-
-	int g_obs[d], g[d];
-	for (j=0; j<d; j++)
-		g_obs[j] = 0;
-
-	*k_obs = 0;
-	for (j=0; j<d; j++)
-		if (sum_obs[j] > delta * (double) N)
-		{
-			(*k_obs)++;
-			g_obs[j] = 1;
-		}
-
-	double E_dist = 0.0;
-	*k_plus = *k_minus = 0.0;
-	*prob_add = 0.0;
-
-	double k_pp, k_mm, prob_aa;
-	k_pp = k_mm = prob_aa = 0.0;
-
-	//#pragma omp parallel for reduction(+: E_dist, k_pp, k_mm, prob_aa)
-	for (i=0; i<N_g; i++)
-	{
-		for (j=0; j<d; j++)
-			g[j] = 0;
-
-		for (l=0; l<genotype[i].k; l++)
-			g[genotype[i].mutation[l]] = 1;
-
-		int dist = 0;
-		int plus = 0;
-		int minus = 0;
-		for (j=0; j<d; j++)
-		{
-			dist += (g[j] != g_obs[j]);
-			plus += (g[j] > g_obs[j]);  // mutation in cell i that is not observable in population
-			minus += (g[j] < g_obs[j]);  // mutation observable, but not present in cell i
-		}
-
-		double frac = (double) genotype[i].count / (double) N;
-		E_dist += frac * (double) dist;
-		k_pp += frac * (double) plus;
-		k_mm += frac * (double) minus;
-		if (plus > 0)
-			prob_aa += frac;
-	}
-
-	*k_plus = k_pp;
-	*k_minus = k_mm;
-	*prob_add = prob_aa;
-
-
-	/*
-double p = 0.0;
-for (k=(*k_obs)+1; k<=*k_max; k++)
-p += k_rel_freq[k];
-	 */
-
-	return E_dist;
-}
 
 
 
@@ -258,14 +87,12 @@ void summary_to_tsv(FILE *DB, int gen)
     //fprintf(DB, "n\tgenotype\n");
 	for (i=0; i<N_g; i++)
 	{
-		fprintf(DB, "%d\t%d\t", gen, genotype[i].count);
+		//fprintf(DB, "%d\t%d\t", gen, genotype[i].count);
 		for (k=0; k<genotype[i].k; k++)
 		{
-			fprintf(DB, "%d", genotype[i].mutation[k]);
-			if(k<genotype[i].k-1)
-				fprintf(DB, ":");
+			fprintf(DB, "%d\t%d\t%d\t%d\n", gen, genotype[i].count, i+1, genotype[i].mutation[k]);
 		}
-		fprintf(DB, "\n");
+		//fprintf(DB, "\n");
 	}
 }
 
@@ -381,7 +208,7 @@ void remove_duplicates()
 
 
 
-int simulate(FILE* DB, int N_init, int N_fin, int gen_max, double u, double v, double s, double s1, int run, int d0, int d1, int verbose)
+int simulate(FILE* DB, int N_init, int N_fin, int gen_max, double u, double v, double s, double s1, int run, int genes, int d0, int d1, int verbose)
 {
 
 	int gen, i, j, c, N; //k;
@@ -444,9 +271,9 @@ int simulate(FILE* DB, int N_init, int N_fin, int gen_max, double u, double v, d
 		{
 			for (j=0;j<genotype[i].k;j++)
 			{
-				if (genotype[i].mutation[j] > 0 && genotype[i].mutation[j] <= d - d0)
+				if (genotype[i].mutation[j] > 0 && genotype[i].mutation[j] <= genes - d0)
 				{  
-					if (genotype[i].mutation[j] <= d - d0 - d1 )
+					if (genotype[i].mutation[j] <= genes - d0 - d1 )
 						fit *= 1 + s;
 					else
 						fit *= 1 + s1;
@@ -480,15 +307,15 @@ int simulate(FILE* DB, int N_init, int N_fin, int gen_max, double u, double v, d
 		for(m=0; m<2; m++){
 			if(m == 0){ // driver
 				mu = u;
-				l = d-d0;
+				l = genes-d0;
 				a = 1.0;
-				b = (double)d - (double)d0 + 1.0;
+				b = (double)genes - (double)d0 + 1.0;
 
 			}else{ // passenger mutation
 				mu = v;
 				l = d0;
-				a = (double)d - (double)d0 + 1.0;
-				b = (double)d + 1.0;
+				a = (double)genes - (double)d0 + 1.0;
+				b = (double)genes + 1.0;
 
 			}
 
@@ -624,51 +451,6 @@ int simulate(FILE* DB, int N_init, int N_fin, int gen_max, double u, double v, d
 
 
 
-void print_pop(FILE* DB, int N, double s, double s_super, int d0)
-{
-	int i, j, k, locus[d], anylocus[d];
-	double fit;
-	for (j=0; j<d; j++)
-		anylocus[j] = 0;
-	for (i=0; i<N_g; i++)
-		for (k=0; k< genotype[i].k; k++)
-			anylocus[genotype[i].mutation[k] - 1]++;
-
-	for (i=0; i<N_g; i++)
-	{
-		for (j=0; j<d; j++)
-			locus[j] = 0;
-		for (k=0; k< genotype[i].k; k++){
-			locus[genotype[i].mutation[k] - 1] = 1;
-		}
-		fit = 1.0;
-
-		for (j=0;j<genotype[i].k;j++)
-		{
-			if (genotype[i].mutation[j] > 0 && genotype[i].mutation[j] <= d)
-			{  
-				if (genotype[i].mutation[j]>5 )
-					fit *= 1 + s;
-				else
-					fit *= 1 + s_super;
-			}
-		}
-
-
-		fprintf(DB, "%g\t", (double)genotype[i].count/(double)N);
-		fprintf(DB, "%d\t", genotype[i].k);
-		fprintf(DB, "%f\t", fit);
-		for (j=0; j<d; j++)
-		{
-			if(j==5 || j==d-d0)
-				fprintf(DB,"\t");
-			//if(anylocus[j]>0)
-			fprintf(DB, "%d", locus[j]);
-		}
-		fprintf(DB, "\n");
-
-	}	
-}
 
 
 
@@ -688,6 +470,7 @@ int main(int argc, char **argv)
 	double s1 = 1.5*s; // Selective advantage of super drivers
 	int    g = 1800;  // number of Generations
 	int    R = 1;  // number of simulations Runs
+	int    d = 1000; // number of driver mutations
 	int    d0 = 0; //number of passengers
 	int    d1 = 0; //number of super drivers
 	char *filestem;
@@ -698,7 +481,7 @@ int main(int argc, char **argv)
 	int f_flag = 0;
 
 	int c = 0;
-	while((c = getopt(argc, argv, "N:n:u:v:s:t:g:R:f:r:p:dh")) != EOF )
+	while((c = getopt(argc, argv, "N:n:u:v:s:t:g:R:f:r:p:d:wh")) != EOF )
 	{
 		switch(c)
 		{
@@ -712,6 +495,13 @@ int main(int argc, char **argv)
 		case 'n':
 			if (atoi(optarg) > 0)
 				N_init = atoi(optarg);
+			else
+				error_flag++;
+			break;
+
+		case 'd':
+			if (atoi(optarg) > 0)
+				d = atoi(optarg);
 			else
 				error_flag++;
 			break;
@@ -743,8 +533,6 @@ int main(int argc, char **argv)
 			else
 				error_flag++;
 			break;
-
-		if(v == -1.0) v=u;
 
 		case 's':
 			if (atof(optarg) >= 0)
@@ -783,7 +571,7 @@ int main(int argc, char **argv)
 			seed = atoi(optarg);
 			break;
 
-		case 'd':
+		case 'w':
 			verbose = 1;
 			break;
 
@@ -796,8 +584,9 @@ int main(int argc, char **argv)
 			printf("  s - Selective advantage (default = %g)\n", s);
 			printf("  t - Selective advantage of other drivers (default = %g)\n", s1);
 			printf("  g - Number of generations (default = %d)\n", g);
+			printf("  d - Number of drivers (default = %d)\n", d);
 			printf("  p - Number of passengers (default = %d)\n", d0);
-			printf("  q - Number of other drivers (default = %d)\n", d1)
+			printf("  q - Number of other drivers (default = %d)\n", d1);
 			printf("  R - Replicates (default = %d)\n", R);
 			printf("  r - Random seed (default = time)\n");
 			printf("  f - File directory (Required! Make sure that the directory exists!)\n");
@@ -809,7 +598,10 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if( d0 + d1 > d) error_flag++;
+	if(v == -1.0)
+		v=u;
+
+	int genes = d + d0 + d1;
 
 	if (error_flag || (! f_flag))
 	{
@@ -837,7 +629,7 @@ int main(int argc, char **argv)
 	for (r=0; r<R; r++)
 	{
 		printf("Sample %i/%i\n", r+1,R);
-		sprintf(summary_filename, "%s/r%03d.R", filestem, r+1);
+		sprintf(summary_filename, "%s/r%03d.csv", filestem, r+1);
 		FILE *DB;
 		if ((DB = fopen(summary_filename, "w")) == NULL)
 		{
@@ -845,20 +637,11 @@ int main(int argc, char **argv)
 			exit(1);
 		}
 		//fprintf(DB, "pop <- list(");
-		simulate(DB, N_init, N, g, u, v, s, s1, r+1, d0, d1, verbose);
+		simulate(DB, N_init, N, g, u, v, s, s1, r+1, genes, d0, d1, verbose);
 		//fprintf(DB, ")");
 
 		fclose(DB);
 
-		//printf("Succeed.\n");
-		//sprintf(filename, "%s/r%03d.pop", filestem, r+1);
-		//if ((DB = fopen(filename, "w")) == NULL)
-		//{
-		//	fprintf(stderr, "In run %d: Cannot open output file -- %s\n", r+1, filename);
-		//	exit(1);
-		//}
-		//print_pop(DB,N, s, s_super, d0);
-		//fclose(DB);
 	}
 
 
